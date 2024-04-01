fd04fa54358971ec7ff7e238ebc7cefc69c0dd93
Андрей <@LAPTOP-5HTBB3CO>
2024-04-01 16:53:47 +0300

    Добавьте файлы проекта.

---
+++ Hello World/Hello World.cpp
@@ -0,0 +1,11 @@
+﻿// Hello World.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+short;
